0fe3fbe8130a461360f16f68763b10ad39dac904
xuant-html <xuan79945@gmail.com>
2025-03-07 23:13:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('greetingButton').addEventListener('click', function() {
+    document.getElementById('greetingMessage').textContent = 'Chúc các bạn một ngày thật ý nghĩa và tràn đầy niềm vui!';
+  });
